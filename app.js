20301cba4b91e49b49a7a53b5e183d5a5f79c3a1
Bucci <s_bucci@tss-hr420-dm24.local>
2018-01-29 13:27:40 -0500

    updating

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express = require('express');
+const express = require('express'); //just like an includes in php
 const app = express();
+const io = require('socket.io')();
+
+app.use(express.static('public'));
 
 //set up routes
 app.use(require('./routes/index'));
@@ -7,6 +10,17 @@ app.use(require('./routes/contact'));
 app.use(require('./routes/users'));
 
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('app running on port 3000!');
+});
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+  console.log('a user has connected');
+  io.emit('connectMsg', { for: 'everyone', msg : `${socket.id} is here!`});
+
+  socket.on('disconnect', () => {
+    console.log('a user has disconnected!');
+  });
 });
