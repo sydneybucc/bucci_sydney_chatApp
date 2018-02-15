c40cdc4150f3295e88e938423501a79e05f919f7
sydneybucc <s_bucci@fanshaweonline.ca>
2018-02-14 19:05:10 -0500

    adding to VSC

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,45 @@
 (() => {
   const socket = io();
+
+  let messageList = document.querySelector('ul'),
+      chatForm = document.querySelector('form'),
+      nameInput = document.querySelector('.nickname'),
+      chatMessage = chatForm.querySelector('.message');
+      nickName = null;
+
+  function setNickname() {
+    //debugger;
+    nickName = this.value;
+  }
+
+  function handleSendMessage(e) {
+    e.preventDefault(); //prevent the default behaviour - a submit triggers a page reload which we dont want
+    //debugger;
+    nickName = (nickName && nickName.length > 0) ? nickName : 'user'; //turnery statemant; an if/else statement in one
+
+    //grab the text from the input field at the bottom of the page
+    msg = `${nickName} says ${chatMessage.value}`;
+
+    //emit a chat event so that we can pass it through to the server (and everyone else)
+    socket.emit('chat message', msg);
+    chatMessage.value = '';
+    return false;
+  }
+
+  function appendMessage(msg) {
+    //debugger;
+    let newMsg = `<li>${msg.message}</li>`;
+    messageList.innerHTML += newMsg;
+  }
+
+  function appendDMessage(msg) {
+    //debugger;
+    let newMsg = `<li>${msg}</li>`;
+    messageList.innerHTML += newMsg;
+  }
+
+  nameInput.addEventListener('change', setNickname, false);
+  chatForm.addEventListener('submit', handleSendMessage, false);
+  socket.addEventListener('connectMsg', appendMessage, false); //make sure the connectMsg name is the same on the app.js
+  socket.addEventListener('disconnect message', appendDMessage, false);
 })();
